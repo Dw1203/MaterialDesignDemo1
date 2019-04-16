524bfa014ea80b6f9f8b50d6bae9e0a6827a059c
dw <782672174@qq.com>
2019-04-16 16:01:20 +0800

    test

---
+++ MaterialDesignDemo1/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MaterialDesignDemo1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("TSCLIB.dll", EntryPoint = "openport")]
+        public static extern int openport(string printername);
+
+        [DllImport("TSCLIB.dll", EntryPoint = "sendcommand")]
+        public static extern int sendcommand(string command);
+
+        [DllImport("TSCLIB.dll", EntryPoint = "about")]
+        public static extern int about();
+
+        [DllImport("TSCLIB.dll", EntryPoint = "usbportqueryprinter")]
+        public static extern int usbportqueryprinter();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StackPanel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        
+    }
+}
